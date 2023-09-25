3b3b162bdfe191c252d59e038989675ef522613a
Bly Kim <blykim@cluml.com>
2023-09-25 16:07:32 +0900

    Modify to `DateTime` from `NaiveDateTime`
    - The format of the datetime should be in accordance with RFC3339

--- src/argument.rs
+++ src/argument.rs
@@ -1,13 +1,13 @@
 use anyhow::{bail, Result};
-use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 pub fn timestamp_to_utc(timestamp: i64) -> DateTime<Utc> {
     Utc.timestamp_nanos(timestamp)
 }
 
 pub fn datetime_to_nanos(datetime: &str) -> Result<i64> {
-    let Ok(datetime) = NaiveDateTime::parse_from_str(datetime, "%Y-%m-%dT%H:%M:%S%.f%z") else {
-        bail!("Invalid datetime format: %Y-%m-%dT%H:%M:%S%.f%z");
+    let Ok(datetime) = DateTime::parse_from_rfc3339(datetime) else {
+        bail!("Invalid rfc3339 datetime format");
     };
     Ok(datetime.timestamp_nanos_opt().unwrap_or(i64::MAX))
 }
